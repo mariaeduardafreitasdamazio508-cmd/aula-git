7f3d9c89ca43d3a4ffa3451dbf675d28bdb97105
mariaeduardafreitasdamazio508-cmd <mariaeduardafreitasdamazio598@gmail.com>
2026-03-18 17:12:25 -0300

    feat(04) : metas de passos

---
+++ src/Ex04.java
@@ -0,0 +1,16 @@
+static void main() {
+    Scanner sc = new Scanner(System.in);
+    int passos, passosrestantes, passosextras, passoreta;
+
+   passoreta=10000;
+
+    IO.println("Digite quantos passos vc deu hoje no total");
+   passos = sc.nextInt();
+
+  if (passos == passoreta) {
+      IO.println("voce atingiu sua meta");
+
+
+
+} else if (passos<passoreta){
+}
