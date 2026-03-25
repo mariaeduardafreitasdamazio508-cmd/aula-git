7746da86986079c037a16ae2a30bc21ad7e7f268
mariaeduardafreitasdamazio508-cmd <mariaeduardafreitasdamazio598@gmail.com>
2026-03-25 17:12:26 -0300

    feat:acabando ex7

---
+++ src/Ex07.java
@@ -0,0 +1,36 @@
+public class Ex07 {
+}
+
+void main() {
+
+
+    Scanner sc = new Scanner(System.in);
+    int notafinal, notarestantes, notarextras, notarmeta;
+
+
+    notarmeta >=7;
+
+    IO.println("qual nota voce tirou");
+    notafinal = sc.nextInt();
+
+    if (notafinal == notarmeta) {
+        IO.println("Voce conseguiu atingir seu objetivo");
+
+
+    }else if (notarmeta < notarmeta) {
+        IO.println("Voce não atingiu seu objetivo faltam : ");
+        notarestantes = notarmeta - notafinal;
+        IO.println("faltam" + notafinal);
+
+
+    }else{
+
+        IO.println("Voce ultrapassou sua meta: ");
+        IO.println(notafinal - 40);}}
+
+
+
+
+
+
+}
