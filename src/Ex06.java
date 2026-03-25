3047930299453259c69af065171efbf216133a93
mariaeduardafreitasdamazio508-cmd <mariaeduardafreitasdamazio598@gmail.com>
2026-03-25 16:44:58 -0300

    feat:exercício06

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,2 +1,34 @@
 public class Ex06 {
 }
+
+void main() {
+    Scanner sc = new Scanner(System.in);
+int estudar, horasrestantes, horasextras, estudarmeta;
+    estudarmeta = 40;
+    IO.println("quantas horas voce estudou hoje");
+    estudar = sc.nextInt();
+
+    if (estudar == estudarmeta) {
+        IO.println("Voce conseguiu atingir seu objetivo");
+
+
+    }else if (estudar < estudarmeta) {
+        IO.println("Voce não atingiu seu objetivo faltam : ");
+        horasrestantes = estudarmeta - estudar;
+        IO.println("faltam" + horasrestantes);
+
+
+    }else{
+
+        IO.println("Voce ultrapassou sua meta: ");
+        IO.println(estudar - 40);}}
+
+
+
+
+
+
+
+
+
+
